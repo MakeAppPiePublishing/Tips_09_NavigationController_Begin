d761e43ad3bb3a599128c9966c4e4be33b9f6422
Steve Lipton <steve@makeapppie.com>
2017-12-05 07:03:02 -0600

    Initial Commit

---
+++ NavigationController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NavigationController
+//
+//  Created by Steven Lipton on 12/5/17.
+//  Copyright © 2017 Steven Lipton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
